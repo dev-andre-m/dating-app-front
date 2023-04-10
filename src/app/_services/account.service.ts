ded35e06f48974bcbdc2bd5a24c92b4ff5b4917a
Andre Mendes <andre.eletrom@hotmail.com>
2023-04-10 15:50:34 -0300

    feat: home and register component

--- src/app/_services/account.service.ts
+++ src/app/_services/account.service.ts
@@ -7,30 +7,42 @@ import { User } from '../models/user';
   providedIn: 'root'
 })
 export class AccountService {
+
   baseUrl = 'http://localhost:5000/api/';
   private currentUserSource = new BehaviorSubject<User | null>(null);
   currentUser$ = this.currentUserSource.asObservable();
 
   constructor(private http: HttpClient) {}
 
-    login(model: any) {
-      return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
-        map((response: User) => {
-          const user = response;
-          if(user){
-            localStorage.setItem('user', JSON.stringify(user));
-            this.currentUserSource.next(user);
-          }
-        })
-      );
-    }
+  login(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
+      map((response: User) => {
+        const user = response;
+        if(user){
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    );
+  }
 
-    setCurrentUser(user: User) {
-      this.currentUserSource.next(user);
-    }
-    
-    logout() {
-      localStorage.removeItem('user');
-      this.currentUserSource.next(null);
-    }
+  register(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+      map(user => {
+        if(user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    )
+  }
+
+  setCurrentUser(user: User) {
+    this.currentUserSource.next(user);
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
+  }
 }
