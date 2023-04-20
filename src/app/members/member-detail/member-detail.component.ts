eb137e6ea9a176410f1f4df9f14987b519417c72
Andre Mendes <andre.eletrom@hotmail.com>
2023-04-20 17:44:42 -0300

    feat: added message button routing to messages

--- src/app/members/member-detail/member-detail.component.ts
+++ src/app/members/member-detail/member-detail.component.ts
@@ -14,8 +14,8 @@ import { MessageService } from 'src/app/_services/message.service';
 })
 export class MemberDetailComponent implements OnInit{
 
-  @ViewChild('memberTabs') memberTabs?: TabsetComponent;
-  member: Member | undefined;
+  @ViewChild('memberTabs', {static: true}) memberTabs?: TabsetComponent;
+  member: Member = {} as Member;
   galleryOptions: NgxGalleryOptions[] = [];
   galleryImages: NgxGalleryImage[] = [];
   activeTab?: TabDirective;
@@ -24,7 +24,15 @@ export class MemberDetailComponent implements OnInit{
   constructor(private memberService: MembersService, private route: ActivatedRoute, private messageService: MessageService) {}
   
   ngOnInit(): void {
-    this.loadMember();
+    this.route.data.subscribe({
+      next: data => this.member = data['member']
+    })
+
+    this.route.queryParams.subscribe({
+      next: params => {
+        params['tab'] && this.selectTab(params['tab'])
+      }
+    });
 
     this.galleryOptions = [{
       width: '500px',
@@ -35,6 +43,8 @@ export class MemberDetailComponent implements OnInit{
       preview: false
       }
     ]
+
+    this.galleryImages = this.getImages();
   }
 
   getImages() {
@@ -50,15 +60,10 @@ export class MemberDetailComponent implements OnInit{
     return imageUrls;
   }
   
-  loadMember() {
-    const username = this.route.snapshot.paramMap.get('username');
-    if(!username) return;
-    this.memberService.getMember(username).subscribe({
-      next: member => {
-        this.member = member
-        this.galleryImages = this.getImages();
-      }
-    })
+  selectTab(heading: string) {
+    if(this.memberTabs) {
+      this.memberTabs.tabs.find(x => x.heading === heading)!.active = true;
+    }
   }
 
   loadMessages() {
