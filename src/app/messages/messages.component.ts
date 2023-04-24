fdf8d1fd908a876bbac135de035af6da1ef79e10
Andre Mendes <andre.eletrom@hotmail.com>
2023-04-24 09:50:23 -0300

    fix: added loading flag to fix transition between inbox and outbox messages

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -14,18 +14,21 @@ export class MessagesComponent implements OnInit {
   container = 'Unread';
   pageNumber = 1;
   pageSize = 5;
+  loading = false;
 
-  constructor(private messageService: MessageService) {}
-  
+  constructor(private messageService: MessageService) { }
+
   ngOnInit(): void {
     this.loadMessages();
   }
 
   loadMessages() {
+    this.loading = true;
     this.messageService.getMessages(this.pageNumber, this.pageSize, this.container).subscribe({
       next: response => {
         this.messages = response.result;
         this.pagination = response.pagination;
+        this.loading = false;
       }
     })
   }
